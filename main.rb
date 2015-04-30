48b7c495f57c5a897a3395ec0366c4370804eb56
Danilo Morales <danny@gadaproductions.com>
2015-04-30 14:53:47 -0400

    posts display working

--- main.rb
+++ main.rb
@@ -13,9 +13,6 @@ def current_user
 	session[:user_id] ? User.find(session[:user_id]) : nil
 end
 
-# def current_post
-# 	session[:post_id] ? Post.find(session[:post_id]) : nil
-# end
 
 get '/'	do
 	erb :home
